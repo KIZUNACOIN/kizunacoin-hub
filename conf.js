73e8d1aeba96c9ebea7e9366d0f93aee5362a6d7
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2018-12-17 12:43:16 +0800

    updated push_notification_part and exchange_rate

--- conf.js
+++ conf.js
@@ -9,9 +9,9 @@ exports.pushApiProjectNumber = 0;
 exports.pushApiKey = '';
 
 // iOS Push Notifications APNS config
-//exports.APNsAuthKey = ''; // *.p8 filepath or buffer with key itself
-//exports.keyId = '';
-//exports.teamId = '';
+exports.APNsAuthKey = ''; // *.p8 filepath or buffer with key itself
+exports.keyId = '';
+exports.teamId = '';
 
 exports.port = 6611;
 //exports.myUrl = 'wss://mydomain.com/bb';
@@ -43,22 +43,6 @@ exports.initial_witnesses = [
     "YG6PVKKDDQL37N6FUHM7ZCJ5ZHD6QGIS"
 ];
 
-/* new testnet
-exports.initial_witnesses = [
-	'2FF7PSL7FYXVU5UIQHCVDTTPUOOG75GX',
-	'2GPBEZTAXKWEXMWCTGZALIZDNWS5B3V7',
-	'4H2AMKF6YO2IWJ5MYWJS3N7Y2YU2T4Z5',
-	'DFVODTYGTS3ILVOQ5MFKJIERH6LGKELP',
-	'ERMF7V2RLCPABMX5AMNGUQBAH4CD5TK4',
-	'F4KHJUCLJKY4JV7M5F754LAJX4EB7M4N',
-	'IOF6PTBDTLSTBS5NWHUSD7I2NHK3BQ2T',
-	'O4K4QILG6VPGTYLRAI2RGYRFJZ7N2Q2O',
-	'OPNUXBRSSQQGHKQNEPD2GLWQYEUY5XLD',
-	'PA4QK46276MJJD5DBOLIBMYKNNXMUVDP',
-	'RJDYXC4YQ4AZKFYTJVCR5GQJF5J6KPRI',
-	'WELOXP3EOA75JWNO6S5ZJHOO3EYFKPIR'
-];
-*/
 
 exports.initial_peers = [
 	'wss://hub.kizunacoin.jp'
--- exchange_price_feed.js
+++ exchange_price_feed.js
@@ -5,84 +5,43 @@ const request = require('request');
 const eventBus = require('core/event_bus.js');
 const network = require('core/network.js');
 
-const symbols = ['USDT-BTC', 'BTC-GBYTE'];
 const rates = {};
 
-function updateBittrexRates(state, onDone) {
-	const apiUri = 'https://bittrex.com/api/v1.1/public/getmarketsummaries';
-	request(apiUri, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			let arrCoinInfos = JSON.parse(body).result;
-			let prices = {};
-			arrCoinInfos.forEach(coinInfo => {
-				if (!coinInfo.Last)
-					return;
-				if (symbols.includes(coinInfo.MarketName)) {
-					prices[coinInfo.MarketName] = coinInfo.Last;
-					console.log("new exchange rate: " + coinInfo.MarketName + "=" + coinInfo.Last);
-				}
-			});
-			if (Object.keys(prices).length == symbols.length) {
-				rates['GBYTE_USD'] = prices['BTC-GBYTE'] * prices['USDT-BTC'];
-				state.updated = true;
-			}
-		}
-		else {
-			console.log("Can't get currency rates from bittrex");
-		}
-		onDone();
-	});
+function updateBitZRates(state, onDone) {
+    const apiUri = 'https://apiv2.bitz.com/Market/coinRate?coins=kiz';
+    request(apiUri, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            let arrCoinInfos = JSON.parse(body).data.kiz;
+            rates['KIZ_USD'] = arrCoinInfos.usdt;
+            state.updated = true;
+        } else {
+            console.log("Can't get currency rates from BitZ");
+        }
+        onDone();
+    });
 }
 
-function updateFreebeRates(state, onDone) {
-	const apiUri = 'http://freebe.byte-ball.com/last';
-	request(apiUri, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			console.log("freebe: ", body);
-			let price;
-			try{
-				price = parseFloat(JSON.parse(body).price_bytes);
-				console.log("GBB/GB = "+price);
-			}
-			catch(e){
-				console.log('bad response from freebe:', e);
-				return onDone();
-			}
-			if (rates['GBYTE_USD'] && price) {
-				rates['GBB_USD'] = rates['GBYTE_USD'] * price;
-				state.updated = true;
-			}
-		}
-		else {
-			console.log("Can't get currency rates from freebe");
-		}
-		onDone();
-	});
+
+function updateRates() {
+    let state = {updated: false};
+    async.series([
+        function (cb) {
+            updateBitZRates(state, cb);
+        },
+    ], function () {
+        console.log(rates);
+        if (state.updated)
+            broadcastNewRates();
+    });
 }
 
-function updateRates(){
-	let state = {updated: false};
-	async.series([
-		function(cb){
-			updateBittrexRates(state, cb);
-		},
-		function(cb){
-			updateFreebeRates(state, cb);
-		}
-	], function(){
-		console.log(rates);
-		if (state.updated)
-			broadcastNewRates();
-	});
+function broadcastNewRates() {
+    network.sendAllInboundJustsaying('exchange_rates', rates);
 }
 
-function broadcastNewRates(){
-	network.sendAllInboundJustsaying('exchange_rates', rates);
-}
-
-eventBus.on('client_logged_in', function(ws){
-	if (Object.keys(rates).length > 0)
-		network.sendJustsaying(ws, 'exchange_rates', rates);
+eventBus.on('client_logged_in', function (ws) {
+    if (Object.keys(rates).length > 0)
+        network.sendJustsaying(ws, 'exchange_rates', rates);
 });
 
 updateRates();
